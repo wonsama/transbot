98e8d4a07884433290491db4d31b68aa23ba9ef5
wonsama <imtough99@gmail.com>
2018-07-05 13:49:58 +0900

    update logs

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const striptags = require('striptags');
 const STEEM_TRANS_AUTHOR = process.env.STEEM_TRANS_AUTHOR;
 const STEEM_TRANS_KEY_POSTING = process.env.STEEM_TRANS_KEY_POSTING;
 
+const CUT_BODY_LENGTH = 5000; // 구글번역은 최대 5000자 까지 가능, 물론 split하면 되지만 5000 글자 자체가 많음.
+const TIME_SEC = 1000;				// 초
+const MONITOR_RELOAD_SEC = 10;	// 버퍼 모니터링 주기
+
 // clear screen
 clear();
 
@@ -20,14 +24,11 @@ clear();
 * 버퍼에 담긴 내용을 모니터링 한다
 */
 let buffers = [];
-let lock = false;
-async function monitorBuffer(sec=5){
+async function monitorBuffer(){
 	
 	let err;
 
-	if(!lock && buffers.length>0){
-		// 작업중에는 잠금처리함.
-		lock = true;
+	if(buffers.length>0){
 
 		// 첫번째로 담긴 내용을 읽어들임
 		let item = buffers[0];
@@ -46,11 +47,11 @@ async function monitorBuffer(sec=5){
 
 			// 댓글 작성하기
 			let reply;
-			let body = `본문(${_getLang(trans.from.language.iso)}) 이(가) ${_getLang(item.cmd)} 로 아래와 같이 번역되었습니다. created by @wonsama\n\n${trans.text}`
+			let body = `본문(${_getLang(trans.from.language.iso)}) 이(가) ${_getLang(item.cmd)} (으)로 아래와 같이 번역되었습니다.\n\n---\n${trans.text}\n\n---\ncreated by @wonsama\n`
 			body = striptags(body, [], '\n');
 			let wif = STEEM_TRANS_KEY_POSTING;
 			let author = STEEM_TRANS_AUTHOR;
-			let permlink = `${author}-trans-${new Date().getTime()}`;
+			let permlink = `${item.author}-trans-${new Date().getTime()}`;
 			let title = '';
 			let jsonMetadata = {
 				tags:['wonsama','transbot'],
@@ -59,26 +60,36 @@ async function monitorBuffer(sec=5){
 			};
 			[err, reply] = await to(steem.broadcast.commentAsync(wif, item.author, item.permlink, author, permlink, title, body, jsonMetadata));
 			if(!err){
-				// 번역된 정보 출력 - 확인용
-				console.log(`translate : https://steemit.com/@${author}/${permlink}`);
 
 				// 버퍼 제거
 				buffers.shift();
-			}else{
-				console.log('reply error',err);
+
+				// 번역된 정보 출력 - 확인용
+				console.log(new Date(), `translate : https://steemit.com/@${author}/${permlink} , current buffer size : ${buffers.length}`);
+				console.log(`--------------------------------------------------`);
+
+				// 다시 N초 이후 버퍼를 재 검색한다 - 정상처리 된 경우임
+				setTimeout(monitorBuffer, MONITOR_RELOAD_SEC * TIME_SEC);	
 			}
-		}else{
-			console.log('get content error',err);
 		}
+		
+	}else{
 
-		// TODO : 각단계에서 오류가 나는 경우 파일로 기록
-		// 부모글조회, 댓글 작성
+		console.log(new Date(), 'buffer is empty');
 
-		// 잠금해제 
-		lock = false;
+		// 다시 N초 이후 버퍼를 재 검색한다 - 버퍼에 담긴 내용이 없는 경우 
+		setTimeout(monitorBuffer, MONITOR_RELOAD_SEC * TIME_SEC);	
 	}
 
-	setTimeout(monitorBuffer, 1000 * sec);
+	// 오류가 발생한 경우
+	if(err){
+		// TODO : 각단계(부모글조회, 댓글 작성)에서 오류가 나는 경우 파일로 기록
+		console.log(new Date(), err);
+
+		// 다시 N초 이후 버퍼를 재 검색한다 - 오류가 담긴 경우 ( 버퍼는 아직 제거 안됨 )
+		setTimeout(monitorBuffer, MONITOR_RELOAD_SEC * TIME_SEC);
+	}
+
 }
 monitorBuffer();
 
@@ -89,7 +100,11 @@ monitorBuffer();
 */
 function monitoring(data, cmd){
 		data.cmd = cmd;
-		buffers.push(data);	
+		buffers.push(data);
+
+		console.log(new Date(), 'monitoring', data);
+		console.log(`current buffer size : ${buffers.length}`);
+		console.log('goto cmd link', `https://steemit.com/@${data.author}/${data.permlink}`);
 }
 
 // start monitoring
@@ -101,8 +116,8 @@ steem.api.streamOperations(function (err, results) {
 		let command = results[0];
 		let data = results[1];	
 		
-		// 댓글만 추출
-		if(command=='comment' && data && data.parent_author && data.parent_author!=''){
+		// 댓글만 추출, 댓글봇이 쓴 글에는 댓글을 달지 않음.
+		if(command=='comment' && data && data.parent_author && data.parent_author!='' && data.parent_author!=STEEM_TRANS_AUTHOR){
 			
 			// @번역해 태그가 포함된 댓글정보만 가져온다
 			let cmd = getLang(data.body);
